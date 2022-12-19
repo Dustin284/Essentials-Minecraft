f3bfb864bad28a4c218265f10a842a7e0ef7516f
dustin-joeleden <dustin.joel.eden@gmail.com>
2022-12-19 15:41:46 +0100

    + Workbench Command

---
+++ src/main/java/commands/Workbench.java
@@ -0,0 +1,32 @@
+package commands;
+
+import Utils.Permissions;
+import Utils.Strings;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import static Utils.Strings.*;
+
+public class Workbench implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player p = (Player) sender;
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(need_user);
+        } else {
+            if (p.hasPermission(Permissions.workbench_use)) {
+                if (args.length == 0) {
+                    p.openWorkbench(null, true);
+                } else {
+                    p.sendMessage(workbench_usage);
+                }
+            } else {
+                p.sendMessage(noperms + " (" + Permissions.workbench_use + ")");
+            }
+        }
+        return false;
+    }
+}
+
