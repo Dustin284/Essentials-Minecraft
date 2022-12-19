291335f3808321fcb55593988b704ef4869ccd16
dustin-joeleden <dustin.joel.eden@gmail.com>
2022-12-19 14:10:26 +0100

    + Fly Command

---
+++ src/main/java/commands/Fly.java
@@ -0,0 +1,41 @@
+package commands;
+
+import Utils.Permissions;
+import Utils.Strings;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Fly implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player p = (Player) sender;
+        if(!(sender instanceof Player)){
+            sender.sendMessage(Strings.need_user);
+            return true;
+        }else{
+            if(p.hasPermission(Permissions.fly_use)){
+                if(args.length == 0) {
+                    p.sendMessage(Strings.fly_usage);
+                }else if(args.length == 1){
+                    if(args[0].equalsIgnoreCase("on")) {
+                        p.setAllowFlight(true);
+                        p.sendMessage(Strings.fly_on);
+                    }else if(args[0].equalsIgnoreCase("off")) {
+                        p.setAllowFlight(false);
+                        p.sendMessage(Strings.fly_off);
+                    }else{
+                        p.sendMessage(Strings.fly_usage);
+                    }
+                }else{
+                    p.sendMessage(Strings.fly_usage);
+                }
+            }else{
+                p.sendMessage(Strings.noperms);
+            }
+        }
+
+        return false;
+    }
+}
