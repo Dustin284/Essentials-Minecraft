e4feb07576043c20195db7a3f80016cffb84e2cd
dustin-joeleden <dustin.joel.eden@gmail.com>
2022-12-19 13:47:28 +0100

    + Add Listener for Quit and Join

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,19 +1,24 @@
 package main;
 
+import Listener.JoinListener;
+import Listener.QuitListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
+        //Import Listener
+        getServer().getPluginManager().registerEvents(new JoinListener(), this);
+        getServer().getPluginManager().registerEvents(new QuitListener(), this);
 
 
+        //Import Commands
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
