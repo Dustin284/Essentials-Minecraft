f0804b9d32b02a38ed30cda6d2005a428abab5ee
dustin-joeleden <dustin.joel.eden@gmail.com>
2022-12-19 13:40:27 +0100

    + Added Prefix, Noperms, need_user (Strings)

---
+++ src/main/java/Utils/Strings.java
@@ -0,0 +1,9 @@
+package Utils;
+
+import org.bukkit.ChatColor;
+
+public class Strings {
+    public static String prefix = ChatColor.GRAY + "[ " + ChatColor.AQUA + "ESSENTIALS" + ChatColor.GRAY + " ] " + ChatColor.RESET;
+    public static String noperms = prefix + ChatColor.RED + "Du hast keine Berechtigung für diesen Befehl!";
+    public static String need_user = prefix + ChatColor.RED + "Du musst ein Spieler sein!";
+}
