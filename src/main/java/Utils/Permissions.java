a296b76dd94440ebea406b89e967ebe9ed734633
dustin-joeleden <dustin.joel.eden@gmail.com>
2022-12-19 15:41:05 +0100

    + Backpack & Workbench Permission

--- src/main/java/Utils/Permissions.java
+++ src/main/java/Utils/Permissions.java
@@ -1,14 +1,15 @@
 package Utils;
 
 public class Permissions {
-
     // Gamemode command
     public static String gm_use = "gm.use";
-
     // Fly command
     public static String fly_use = "fly.use";
-
     //Heal commands
     public static String heal_use = "heal.use";
     public static String heal_other = "heal.other";
+    //Backpack command
+    public static String backpack_use = "backpack.use";
+    //Workbench command
+    public static String workbench_use = "workbench.use";
 }
